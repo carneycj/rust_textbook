2b98d3128b5e9234b5e02bcd379bd1d7b30e6268
Christopher Carney <carneycj96@gmail.com>
2021-05-22 17:45:50 -0400

    Started ch 4, understanding ownership in rust

---
+++ understanding_ownership/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // s is not valid yet, since it has not been declared
+    // This is a string literal
+    let s = "hello"; // Now it's declared, so it is valid from this point forward
+} // s is not valid, because it is out of scope now
+
+// Pick up from the String type
