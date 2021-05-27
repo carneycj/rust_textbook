484db13a637111070ddacd35aea99de38fdfbf86
Christopher Carney <carneycj96@gmail.com>
2021-05-26 22:02:54 -0400

    Completed ch 4- ownership and borrowing
    This covered ownership and borrowing, and then slicing strings and
    arrays

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     data_interactions();
     return_vals_and_scope();
     using_references();
+    understanding_slicing();
 } // _s is not valid, because it is out of scope now
 
 // Here we show the String type and why it's useful
@@ -26,7 +27,7 @@ fn data_interactions() {
     // This creates a copy of x (even if we didn't use mut in the let statement)
     let mut y = x;
     y += 1;
-    println!("x: {}, y: {}", x, y);
+    println!("\nx: {}, y: {}", x, y);
 
     // The String type has three components-
     // 1: Pointer to the actual value of the string (the actual value of the
@@ -69,7 +70,7 @@ fn return_vals_and_scope() {
                                        // After this function (data_interactions), s3 goes out of scope and is
                                        // dropped. s2 goes out of scope but was moved, so nothing happens. s1 goes
                                        // out of scope and is dropped
-    println!("{}, {}", s1, s3);
+    println!("\n{}, {}", s1, s3);
     let (s4, length) = returns_tuples(s3);
     println!("{}, {}", s4, length);
 }
@@ -98,7 +99,21 @@ fn using_references() {
     // take ownership of the variable. This is important because it doesn't
     // delete the variable once out of scope. This is called borrowing
     let len = calculate_length(&s1);
-    println!("The length of {} is {}", s1, len);
+    println!("\nThe length of {} is {}", s1, len);
+
+    let mut s = String::from("hello");
+    // This is how we allow a reference to make changes to the variable that
+    // it's borrowing.
+    change(&mut s);
+
+    // We also can't have a mutable reference used at the same time as immutable
+    // references.  The immutable references don't expect the value to change
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, {}", r1, r2);
+    // This is fine because we don't use the immutable references any more
+    let r3 = &mut s;
+    println!("{}", r3);
 }
 
 // It's important to add the & to the type when denoting a reference
@@ -107,4 +122,63 @@ fn calculate_length(s: &String) -> usize {
     s.len()
 } // Since s doesn't own the variable, it does not get deleted once out of scope
 
-// Pick up at mutable references
+// We need to include the &mut, since we're changing the value
+fn change(some_string: &mut String) {
+    // We can't have mutable references to a single variable at the same time
+    // This prevents data racing, which is when:
+    // - Two or more pointers access the same data at the same time
+    // - At least one of the pointers is being used to write the data
+    // - There's no mechanism being used to synchronize access to the data
+    some_string.push_str(", world");
+}
+
+fn understanding_slicing() {
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string);
+    // my_string.clear(); // This needs mut
+    // clear makes my_string = "", so the length of the first is no longer 5,
+    // but without string slicing, word doesn't change, and there is no compile
+    // error
+    println!("\n{}", word);
+    // A better solution is string slicing:
+    let s: String = String::from("hello world");
+    // This is a reference to a portion of a string. It is [start: end + 1]. If
+    // the start = 0 or if the end = length of string, that value can be dropped
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}", hello, world);
+
+    let _word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+    let _word = first_word(&my_string_literal[..]);
+    let _word = first_word(my_string_literal);
+
+    // We can also slice arrays:
+    let a = [1, 2, 3, 4, 5];
+    let _slice = &a[1..3];
+}
+
+fn first_word(s: &str) -> &str {
+    // Use type usize for not slicing
+    // This function finds the first word in a string
+    // In order to search the string for a character, we convert to an array of
+    // bytes and iterate through it
+    let bytes = s.as_bytes();
+
+    // The first item in the tuple of enumerate is the index, and the second is
+    // a reference to the item that the iter method returns (so we need the &)
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // return i; // This is how it would be done without slicing
+            return &s[0..i];
+        }
+    }
+
+    // If there is no space, the length of the entire string is returned
+    // s.len() // If slicing wasn't used
+    &s[..]
+    // The return value of this function is literally just the index of the end of
+    // the first word.  If the string changes, this function doesn't change, so
+    // everything goes wrong, even though this doesn't create a compile error
+}
