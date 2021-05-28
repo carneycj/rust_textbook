96f7cf8556cf729865eb84aebaf6818d01afc25a
Christopher Carney <carneycj96@gmail.com>
2021-05-27 22:15:34 -0400

    Completed ch 5, structs
    This ch discussed structs and impl blocks.  This went into using both
    methods and associated functions with structs to better structure your
    code for easier comprehension.

--- using_structs/src/main.rs
+++ using_structs/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     using_structs();
+    using_tuple_structs();
+
+    // Unit-like structs don't have fields.  This is useful when implementing
+    // traits on some type but don't have any data to store in it
 }
 
 struct User {
+    // We can use &str, but that requires lifetimes to be able to implement
     username: String,
     email: String,
     sign_in_count: u64,
@@ -23,6 +28,14 @@ fn using_structs() {
     let email = String::from("an_email@email.com");
     let username = String::from("some_user");
     let user2 = build_user(email, username);
+
+    let user2 = User {
+        email: String::from("another@email.com"),
+        username: String::from("yet_another_username"),
+        ..user1 // This is the same as:
+                // active: user1.active,
+                // sign_in_count: user1.sign_in_count
+    };
 }
 
 fn build_user(email: String, username: String) -> User {
@@ -34,4 +47,12 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
-// Pick up at: Creating Instances From Other Instances With Struct Update Syntax
+fn using_tuple_structs() {
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    // black and origin are different types, because they're instances of
+    // different tuple structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
