de266f0655eda87fba7b0b327164c3f302696664
Christopher Carney <carneycj96@gmail.com>
2021-09-23 20:49:18 -0400

    Finished ch 10.1
    Discussion on generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -39,6 +39,11 @@ fn exploring_generics() {
 
     // Generics are helpful, but if you use too many in one function, you may
     // want to find a way to break it into smaller bits for readability's sake
+
+    use_point();
+    use_point2();
+
+    are_gens_slow();
 }
 
 // Turning this into a function reduces repetition in the code.
@@ -74,16 +79,16 @@ fn largest_char(list: &[char]) -> char {
 // 'T'.  This function has one parameter named 'list', which is a slice of
 // values of type 'T'.  The 'largest' function will return a value of the same
 // type 'T'
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+// fn largest<T>(list: &[T]) -> T {
+// let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+// for &item in list {
+//     if item > largest {
+//         largest = item;
+//     }
+// }
+// largest
+// }
 
 // We can also apply generics to structs:
 // In this case, x and y must be the same type
@@ -98,6 +103,47 @@ struct Point2<T, U> {
     y: U,
 }
 
+// We have to use the <T> right after impl to specify that we're implementing
+// methods on the type Point<T>.  Doing this is what tells the compiler that T
+// is a generic and not a concrete type.
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// This one uses a concrete type
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn use_point() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+}
+
+impl<T, U> Point2<T, U> {
+    // Some generics are defined at the impl level, others at the fn level
+    // We use V and W at this level because it only applies to this function
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn use_point2() {
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
 // We can also do this with enums:
 enum Option<T> {
     Some(T),
@@ -107,4 +153,23 @@ enum Option<T> {
 enum Result<T, E> {
     Ok(T),
     Err(E),
+}
+
+fn are_gens_slow() {
+    // Generics do not slow down rust
+    let integer = Some(5);
+    let float = Some(5.0);
+
+    enum Option_i32 {
+        Some(i32),
+        None,
+    }
+
+    enum Option_f64 {
+        Some(f64),
+        None,
+    }
+
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
 }
