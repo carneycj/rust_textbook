5ed40b3daa9a889430169d631a52da0e69c92077
Christopher Carney <carneycj96@gmail.com>
2023-03-14 23:59:16 -0400

    worked a bit more on ch 13

--- ch13_fn_features/src/main.rs
+++ ch13_fn_features/src/main.rs
@@ -1,5 +1,23 @@
+// RESUME: 13.1, Capturing the Environment with Closures
+
 fn main() {
-    println!("Hello World");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -16,5 +34,21 @@ impl Inventory {
     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
         user_preference.unwrap_or_else(|| self.most_stocked())
     }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
 }
-// RESUME: 13.1, Capturing the Environment with Closures
